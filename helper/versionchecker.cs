f5a22e6a00b96966a86f0c36fe84630882139a2a
Woa6627 <reecestephens99@gmail.com>
2025-04-21 22:35:35 +0100

    new version checker

---
+++ helper/versionchecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoaModHarmInstallerGUI.helper
+{
+    public class versionchecker
+    {
+        public static async Task<string> CheckLatestVersion()
+        {
+            HttpClient client = new HttpClient();
+            try
+            {
+                HttpResponseMessage res = await client.GetAsync("");
+                string content = await res.Content.ReadAsStringAsync();
+                return content;
+            }catch(Exception ex)
+            {
+                MAIN.AppendSTDOUT(ex.Message, true);
+                return "";
+            }
+        }
+    }
+}
